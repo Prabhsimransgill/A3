6c16135f2c29e10342b4c2efb46bff48cd7e8d86
Prabhsimransgill <52840261+Prabhsimransgill@users.noreply.github.com>
2019-07-29 23:53:23 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,11 @@
+import {BookInfo} from '../../app/bookInfo';
+
+export const Mybooks : BookInfo [] =
+[
+   
+    {name: 'Java 2', author: 'Prabh', genre: 'Action', year: '2-6-2007',picture:"assets/images/1.jpg"},
+    {name: 'Linux', author: 'Mehak', genre: 'Romantic', year: '9-7-2017',picture:"assets/images/2.jpg"},
+    {name: 'Database', author: 'Raman', genre: 'Horror', year: '29-5-2017',picture:"assets/images/3.jpg"},
+    {name: 'Web', author: 'Pradeep', genre: 'Comedy', year: '29-7-2016',picture:"assets/images/4.jpg"}
+    
+]
